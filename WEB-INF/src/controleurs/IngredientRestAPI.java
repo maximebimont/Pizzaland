7cce03c2129280fee9f522096f37c392f42b9a3c
Maxime Bimont <maxime.bimont.etu@univ-lille.fr>
2023-02-08 16:35:37 +0100

    Partie 1 : Les ingrédients

--- WEB-INF/src/controleurs/IngredientRestAPI.java
+++ WEB-INF/src/controleurs/IngredientRestAPI.java
@@ -24,32 +24,36 @@ public class IngredientRestAPI extends HttpServlet {
 	IngredientDAO dao = new IngredientDAO();
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.setContentType("application/json;charset=UTF-8");
 		PrintWriter out = res.getWriter();
-		ObjectMapper objectMapper = new ObjectMapper();
-
-		String pathInfo = req.getPathInfo();
-		if (pathInfo == null || pathInfo.equals("/")) {
-			Collection<Ingredient> models = dao.findAll();
-			String jsonstring = objectMapper.writeValueAsString(models);
-			out.print(jsonstring + "\n");
+        res.setContentType("applications/json");
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        String pathInfo = req.getPathInfo();
+        if (pathInfo == null || pathInfo.equals("/")) {
+        	Collection<Ingredient> ingredients = dao.findAll();
+        	String jsonstring = objectMapper.writeValueAsString(ingredients);
+        	out.print(jsonstring);
+        	return;
+        }
+        
+        String[] splits = pathInfo.split("/");
+        if (splits.length != 2 && (splits.length != 3 || (splits.length == 3 && !splits[2].equals("name")))) {
+	        res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+        }
+        
+        String id = splits[1];
+		if (dao.findById(Integer.parseInt(id))==null) {
+			res.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
 		}
-
-		String[] splits = pathInfo.split("/");
-		if (splits.length != 2) {
-			res.sendError(HttpServletResponse.SC_BAD_REQUEST); // 400
-			return;
+		if(splits.length == 2) {
+			out.print(objectMapper.writeValueAsString(dao.findById(Integer.parseInt(id))));
+		} else {
+			Ingredient ing = dao.findById(Integer.parseInt(id));
+			out.print(ing.getName());
 		}
-		int id = Integer.parseInt(splits[1]);
-		if (dao.findById(id) == null) {
-			System.out.println("pas trouve");
-			res.sendError(HttpServletResponse.SC_NOT_FOUND); // 404
-			return;
-		}
-		String jsonstring2 = objectMapper.writeValueAsString(dao.findById(id));
-		out.print(jsonstring2 + "\n");
-		return;
+        return;
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
